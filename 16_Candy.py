c9b239e6a87562cae07e5ecfc430ae78c5a814f9
Andrew G <118138406+andrew0570@users.noreply.github.com>
2024-11-05 15:43:22 -0600

    Update 16_Candy.py

--- 16_Candy.py
+++ 16_Candy.py
@@ -1,6 +1,10 @@
 """
     Since Joey always goes first, he will always win and get more candy
     if both players play optimally
+
+    When there are 2 piles, the first player can always take the larger pile.
+    When there are 4 piles, the first player can either take piles 1+3 or 2+4, and one of the groups will always be larger.
+    This concept can be extended to every number after where the first player takes all the even indexed groups or all the odd indexed groups.
 """
 
 print("Joey")
