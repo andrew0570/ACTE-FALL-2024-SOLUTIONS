9cda448f0ee9da80f358f417d489e43585a1f594
krishnamcm <maisuriakrishnam@gmail.com>
2024-11-11 12:31:41 -0600

    Update 16_Candy.py

--- 16_Candy.py
+++ 16_Candy.py
@@ -6,9 +6,9 @@
     When there are 4 piles, the first player can either take piles 1+3 or 2+4, and one of the groups will always be larger.
     This concept can be extended to every number after where the first player takes all the even indexed groups or all the odd indexed groups.
 """
-#input()
-#input()
-#print("Joey")
+input()
+input()
+print("Joey")
 
 
 """
@@ -18,19 +18,19 @@
      - if joey has positive points from 0 to N then he wins
 """
 # GETTING INPUT
-n = int(input())
-piles = [int(i) for i in input().split()]
+# n = int(input())
+# piles = [int(i) for i in input().split()]
 
 # DEFINE dp(i,j) FUNCTION
-def dp(i,j):
-    if i>j: return 0
+# def dp(i,j):
+#     if i>j: return 0
 
-    if (j-i) % 2 == 1: # joey (player 1)
-        return max(dp(i+1,j)+piles[i], dp(i,j-1)+piles[j])
-    else: # john (player 2)
-        return min(dp(i+1,j)-piles[i], dp(i,j-1)-piles[j])
+#     if (j-i) % 2 == 1: # joey (player 1)
+#         return max(dp(i+1,j)+piles[i], dp(i,j-1)+piles[j])
+#     else: # john (player 2)
+#         return min(dp(i+1,j)-piles[i], dp(i,j-1)-piles[j])
     
-if dp(0,n-1) > 0:
-    print("Joey")
-else:
-    print("John")
+# if dp(0,n-1) > 0:
+#     print("Joey")
+# else:
+#     print("John")
